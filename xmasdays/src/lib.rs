de596ad593c49ddeefd9446507ba97ba9cf5af1f
Roy Evangelista <royevangelista@gmail.com>
2021-06-04 22:09:35 +0800

    December is comming

---
+++ xmasdays/src/lib.rs
@@ -0,0 +1,178 @@
+pub fn lyrics() -> String {
+    let mut lyrics = String::new();
+
+    let days = [
+        "first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth",
+    ];
+
+    let gifts = [
+        "A partridge in a pear tree",
+        "Two turtle doves",
+        "Three French hens",
+        "Four calling birds",
+        "Five gold rings",
+        "Six geese a laying",
+        "Seven swans a swimming",
+        "Eight maids a milking",
+        "Nine ladies dancing",
+        "Ten lords a leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    let mut count = 0;
+
+    for day in days.iter() {
+        lyrics.push_str("On the ");
+        lyrics.push_str(day);
+        lyrics.push_str(" day of Christmas my true love gave to me\n");
+
+        let lines = &gifts[..count+1];
+        let mut c = 0;
+
+        for l in lines.iter().rev() {
+            if count > 0 && count == c {
+                lyrics.push_str(" and\n");
+            }
+
+            lyrics.push_str(l);
+
+            if count > 1 && c < count - 1 {
+                lyrics.push_str("\n");
+            }
+
+            c += 1;
+        }
+
+        count += 1;
+
+        if count < days.len() {
+            lyrics.push_str("\n\n");
+        }
+    }
+
+    return lyrics;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let song = "On the first day of Christmas my true love gave to me
+A partridge in a pear tree
+
+On the second day of Christmas my true love gave to me
+Two turtle doves and
+A partridge in a pear tree
+
+On the third day of Christmas my true love gave to me
+Three French hens
+Two turtle doves and
+A partridge in a pear tree
+
+On the fourth day of Christmas my true love gave to me
+Four calling birds
+Three French hens
+Two turtle doves and
+A partridge in a pear tree
+
+On the fifth day of Christmas my true love gave to me
+Five gold rings
+Four calling birds
+Three French hens
+Two turtle doves and
+A partridge in a pear tree
+
+On the sixth day of Christmas my true love gave to me
+Six geese a laying
+Five gold rings
+Four calling birds
+Three French hens
+Two turtle doves and
+A partridge in a pear tree
+
+On the seventh day of Christmas my true love gave to me
+Seven swans a swimming
+Six geese a laying
+Five gold rings
+Four calling birds
+Three French hens
+Two turtle doves and
+A partridge in a pear tree
+
+On the eighth day of Christmas my true love gave to me
+Eight maids a milking
+Seven swans a swimming
+Six geese a laying
+Five gold rings
+Four calling birds
+Three French hens
+Two turtle doves and
+A partridge in a pear tree
+
+On the ninth day of Christmas my true love gave to me
+Nine ladies dancing
+Eight maids a milking
+Seven swans a swimming
+Six geese a laying
+Five gold rings
+Four calling birds
+Three French hens
+Two turtle doves and
+A partridge in a pear tree
+
+On the tenth day of Christmas my true love gave to me
+Ten lords a leaping
+Nine ladies dancing
+Eight maids a milking
+Seven swans a swimming
+Six geese a laying
+Five gold rings
+Four calling birds
+Three French hens
+Two turtle doves and
+A partridge in a pear tree
+
+On the eleventh day of Christmas my true love gave to me
+Eleven pipers piping
+Ten lords a leaping
+Nine ladies dancing
+Eight maids a milking
+Seven swans a swimming
+Six geese a laying
+Five gold rings
+Four calling birds
+Three French hens
+Two turtle doves and
+A partridge in a pear tree
+
+On the twelfth day of Christmas my true love gave to me
+Twelve drummers drumming
+Eleven pipers piping
+Ten lords a leaping
+Nine ladies dancing
+Eight maids a milking
+Seven swans a swimming
+Six geese a laying
+Five gold rings
+Four calling birds
+Three French hens
+Two turtle doves and
+A partridge in a pear tree";
+
+        assert_eq!(song, lyrics().as_str());
+    }
+}
---
+++ xmasdays/src/main.rs
@@ -0,0 +1,7 @@
+use xmasdays::lyrics;
+
+fn main() {
+    println!("Twelve Days of Christmas");
+    println!("by John Denver, The Muppets\n");
+    println!("{}", lyrics());
+}
