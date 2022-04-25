a4fb59f9dfbdde62ea821e703958b795ab3bd84a
Roy Evangelista <royevangelista@gmail.com>
2022-04-25 19:21:22 +0800

    At the center of median!

--- mathm3/src/main.rs
+++ mathm3/src/main.rs
@@ -2,15 +2,27 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn mean(list:Vec<i32>) -> f64 {
-    let mut total:i32 = 0;
+fn mean(list: &Vec<i32>) -> f64 {
+    let mut total: i32 = 0;
+
+    // NOTE: count is being because I struggled to convert usize to f64.
     let mut count: i32 = 0;
-    for v in &list {
+    for v in list {
         total += *v;
         count += 1;
     }
 
     f64::from(total) / f64::from(count)
+}
+
+fn median(mut list: Vec<i32>) -> f64 {
+    list.sort_unstable();
+    let len = list.len();
+    let mid = len / 2;
+    if len % 2 == 0 {
+        return f64::from(&list[mid - 1] + &list[mid]) / 2.0;
+    }
+    f64::from(list[mid])
 }
 
 #[cfg(test)]
@@ -19,10 +31,18 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let v = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        assert_eq!(mean(v), 4.5);
+        let v = vec![1, 2, 4, 7, 5, 6, 8, 3];
+        assert_eq!(mean(&v), 4.5);
+        assert_eq!(median(v), 4.5);
 
-        let v = vec![9, 10, 12, 13, 13, 13, 15, 15, 16, 16, 18, 22, 23, 24, 24, 25];
-        assert_eq!(mean(v), 16.75);
+        let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        assert_eq!(mean(&v), 5.0);
+        assert_eq!(median(v), 5.0);
+
+        let v = vec![
+            9, 10, 12, 13, 13, 13, 15, 15, 16, 16, 18, 22, 23, 24, 24, 25,
+        ];
+        assert_eq!(mean(&v), 16.75);
+        assert_eq!(median(v), 15.5);
     }
 }
