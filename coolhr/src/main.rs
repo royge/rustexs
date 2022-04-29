565bd7bf40e6f3e1d7fe403971b01b9f6cb9c8a5
Roy Evangelista <royevangelista@gmail.com>
2022-04-29 19:19:46 +0800

    Add registration process

--- coolhr/src/main.rs
+++ coolhr/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Cool HR!");
 }
@@ -24,6 +26,11 @@ fn process(cmd: String) -> (String, String) {
     (dept, emp)
 }
 
+fn register(emp: &String, dept: &String, coy: &mut HashMap<String, Vec<String>>) {
+    let v = coy.entry(dept.to_string()).or_insert(Vec::new());
+    v.push(emp.to_string());
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -36,5 +43,38 @@ mod tests {
             process(cmd.to_string()),
             (String::from("Engineering"), String::from("Tim"))
         );
+
+        let cmd = "Add Kate to Sales";
+
+        assert_eq!(
+            process(cmd.to_string()),
+            (String::from("Sales"), String::from("Kate"))
+        );
+
+        let rob = "Rob";
+        let sheryl = "Sheryl";
+        let john = "John";
+        let steve = "Steve";
+
+        let engg = "Engineering";
+        let mktg = "Marketing";
+
+        let mut coy: HashMap<String, Vec<String>> = HashMap::new();
+        let marketers = vec![sheryl.to_string(), john.to_string(), steve.to_string()];
+        coy.insert(engg.to_string(), vec![rob.to_string()]);
+        coy.insert(mktg.to_string(), marketers);
+
+        let mut results: HashMap<String, Vec<String>> = HashMap::new();
+        let entries = [
+            (rob, engg),
+            (sheryl, mktg),
+            (john, mktg),
+            (steve, mktg),
+        ];
+        for (emp, dept) in &entries {
+            register(&emp.to_string(), &dept.to_string(), &mut results);
+        }
+
+        assert_eq!(results, coy)
     }
 }
