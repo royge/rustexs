648f0b15858b4e406ce8937885caa3a2450a593c
Roy Evangelista <royevangelista@gmail.com>
2023-01-15 16:02:35 +0800

    Convert loop in iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -26,7 +26,7 @@ impl Config {
         })
     }
 
-    fn parse<'a>(
+    fn parse(
         mut args: impl Iterator<Item = String>,
     ) -> Result<(String, String, bool), &'static str> {
         args.next();
@@ -67,27 +67,15 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query.to_lowercase()) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
