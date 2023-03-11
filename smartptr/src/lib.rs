78da59b350130646c9e6e91ce7dc2635e3dc9070
Roy Evangelista <royevangelista@gmail.com>
2023-03-11 16:27:26 +0800

    Pointer is getting smarter

---
+++ smartptr/src/lib.rs
@@ -0,0 +1,75 @@
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage = self.value as f64 / self.max as f64;
+
+        if percentage >= 1.0 {
+            self.messenger.send("Error: Quota reached!");
+        } else if percentage >= 0.9 {
+            self.messenger.send("Warning: Qouta almost reached!");
+        } else if percentage >= 0.75 {
+            self.messenger.send("Warning: 75% quota reached!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, msg: &str) {
+            self.sent_messages.borrow_mut().push(String::from(msg));
+            // let mut borrow_one = self.sent_messages.borrow_mut();
+            // let mut borrow_two = self.sent_messages.borrow_mut();
+
+            // borrow_one.push(String::from(msg));
+            // borrow_two.push(String::from(msg));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let msgr = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&msgr, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(msgr.sent_messages.borrow().len(), 1);
+    }
+}
--- smartptr/src/main.rs
+++ smartptr/src/main.rs
@@ -1,4 +1,7 @@
 use crate::List::{Cons, Nil};
+use crate::rc::List::{Cons as RcCons, Nil as RcNil};
+use crate::rccell::List::{Cons as RcCellCons, Nil as RcCellNil};
+use std::cell::RefCell;
 use std::fmt;
 use std::ops::Deref;
 use std::rc::Rc;
@@ -7,10 +10,10 @@ fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
-    // let c = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-    // let _d = Cons(4, Box::new(c));
+    let c = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let _d = Cons(4, Box::new(c));
 
-    // // This can cause compile error.
+    // This can cause compile error.
     // let _e = Cons(5, Box::new(c));
 
     let x = 5;
@@ -40,16 +43,19 @@ fn main() {
     drop(b);
     println!("End of main");
 
-    let c = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil)))))));
+    let c = Rc::new(RcCons(
+        1,
+        Rc::new(RcCons(2, Rc::new(RcCons(3, Rc::new(RcNil))))),
+    ));
     println!("ref count of c = {}", Rc::strong_count(&c));
     {
-        let _d = Cons(4, Rc::clone(&c));
+        let _d = RcCons(4, Rc::clone(&c));
         println!(
             "ref count of c after creating _d = {}",
             Rc::strong_count(&c)
         );
         {
-            let _e = Cons(5, Rc::clone(&c));
+            let _e = RcCons(5, Rc::clone(&c));
             println!(
                 "ref count of c after creating _e = {}",
                 Rc::strong_count(&c)
@@ -64,17 +70,45 @@ fn main() {
         "ref count of c after _d is out of scope = {}",
         Rc::strong_count(&c)
     );
+
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(RcCellCons(Rc::clone(&value), Rc::new(RcCellNil)));
+
+    let b = RcCellCons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = RcCellCons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
 
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(i32, Box<List>),
     Nil,
 }
 
-// enum List {
-//     Cons(i32, Box<List>),
-//     Nil,
-// }
+mod rc {
+    use std::rc::Rc;
+
+    pub enum List {
+        Cons(i32, Rc<List>),
+        Nil,
+    }
+}
+
+mod rccell {
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
+    #[derive(Debug)]
+    pub enum List {
+        Cons(Rc<RefCell<i32>>, Rc<List>),
+        Nil,
+    }
+}
 
 fn hello(name: &str) {
     println!("Hello, {}", name)
