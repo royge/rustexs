b51688623fd79761548dddaa7292f51f64960fe9
Roy Evangelista <royevangelista@gmail.com>
2023-03-11 10:48:42 +0800

    A pointer is considered smart if it can count references

--- smartptr/src/main.rs
+++ smartptr/src/main.rs
@@ -1,12 +1,17 @@
 use crate::List::{Cons, Nil};
-use std::ops::Deref;
 use std::fmt;
+use std::ops::Deref;
+use std::rc::Rc;
 
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
-    let _ = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let c = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let _d = Cons(4, Box::new(c));
+
+    // // This can cause compile error.
+    // let _e = Cons(5, Box::new(c));
 
     let x = 5;
     let y = &x;
@@ -34,12 +39,42 @@ fn main() {
     let b = MyBox::new(100);
     drop(b);
     println!("End of main");
+
+    let c = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil)))))));
+    println!("ref count of c = {}", Rc::strong_count(&c));
+    {
+        let _d = Cons(4, Rc::clone(&c));
+        println!(
+            "ref count of c after creating _d = {}",
+            Rc::strong_count(&c)
+        );
+        {
+            let _e = Cons(5, Rc::clone(&c));
+            println!(
+                "ref count of c after creating _e = {}",
+                Rc::strong_count(&c)
+            );
+        }
+        println!(
+            "ref count of c after _e is out of scope = {}",
+            Rc::strong_count(&c)
+        );
+    }
+    println!(
+        "ref count of c after _d is out of scope = {}",
+        Rc::strong_count(&c)
+    );
 }
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
+
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
 
 fn hello(name: &str) {
     println!("Hello, {}", name)
